1044fbb95294913967d37e93a1229d3129bc8d8f
berkut <berkut_cool@mail.ru>
2022-09-16 19:03:53 +0400

    added method ShowArray

--- Program.cs
+++ Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void ShowArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write(arr[i] + " ");
+    }
+}
+
+
+
+Console.WriteLine("Input size array: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
