d4a344483d500ff30c6eaf87de453c163a7ac576
berkut <berkut_cool@mail.ru>
2022-09-16 19:11:33 +0400

    added method CreateArray

--- Program.cs
+++ Program.cs
@@ -6,7 +6,18 @@
     }
 }
 
+string[] CreateArray(int size)
+{
+    string[] arr = new string[size];
 
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.WriteLine($"Input the {i + 1} element of the array: ");
+        arr[i] = Console.ReadLine();
+    }
+
+    return arr;
+}
 
 Console.WriteLine("Input size array: ");
 int n = Convert.ToInt32(Console.ReadLine());
